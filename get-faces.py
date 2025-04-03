4a1ad6d2f2440667da5666e6ce1e61130143df85
Angel <acherfun99@gmail.com>
2025-04-03 18:10:42 -0400

    adding extractor image from video

---
+++ get-faces.py
@@ -0,0 +1,42 @@
+import cv2
+import os
+import imutils
+
+personName = 'example'
+dataPath = './data'
+personPath= dataPath+'/'+personName
+
+if not os.path.exists(personPath):
+    print("A directory was created: ",personPath)
+    os.makedirs(personPath)
+
+cap = cv2.VideoCapture('./db-videos/example.mp4')
+
+faceClassIf = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+count = 0
+
+while True:
+    ret, frame = cap.read()
+    if ret == False: break
+    frame = imutils.resize(frame,width=300, height=360)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    auxFrame = frame.copy()
+
+    faces = faceClassIf.detectMultiScale(gray,1.4,5)
+    print("faces",faces)
+    
+    for (x,y,w,h) in faces:
+        
+        cv2.rectangle(frame, (x,y), (x+w,+h),(0,255,0),2)
+        face2 = auxFrame[y:y+h,x:x+w]
+        face = cv2.resize(face2,(150,150),interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(personPath + '/rostro_{}.jpg'.format(count),face)
+        count = count +1
+    cv2.imshow('frame',frame)
+
+    k= cv2.waitKey(1)
+    if k == 27 or count >= 300:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
